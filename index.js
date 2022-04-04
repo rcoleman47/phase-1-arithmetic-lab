c5e8d2fb9eb0f7c4162d81562b33ed21f75591c3
ryanadams <rcoleman.adams@gmail.com>
2022-04-04 16:22:46 -0500

    commit tree 8ebe8645cf39a0b3fe9e56f105b2ad62fa670cc9

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;  
+const multiply = num1 * num2;
+
+const random = Math.round((Math.random()*10));
+
+let num3 = 16;
+let num4 = 6;
+
+const mod = num3%=num4;
+
+const max = Math.max(1, 2, 11, 16, 20, 5, 14, 3, 10 ,19);
